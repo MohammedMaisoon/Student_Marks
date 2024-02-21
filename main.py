943edda7da514d50a0818f0ab7b5f545fb2ae241
Mohammed <Sharks@macusers-MacBook-Pro.local>
2024-02-21 15:27:04 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+marks=int(input("Ente Your Marks : "))
+if marks>85 and marks<=100:
+    print("You are Grade A.")
+elif marks>60 and marks<=85:
+    print("You Are Grade B.")
+elif marks>40 and marks<=60:
+    print("You are Grade C.")
+else:
+    print("You Are Fail...")
